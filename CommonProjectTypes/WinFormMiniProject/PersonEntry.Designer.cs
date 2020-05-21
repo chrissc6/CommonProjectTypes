310f6c2552f1ef479dd5fe94f78d989a137011e0
chris <christopherscaudill@gmail.com>
2020-05-21 13:06:59 -0400

    winform miniproject pt2

--- CommonProjectTypes/WinFormMiniProject/PersonEntry.Designer.cs
+++ CommonProjectTypes/WinFormMiniProject/PersonEntry.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormMiniProject
 {
-    partial class Form1
+    partial class PersonEntry
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.isActiveCheckBox = new System.Windows.Forms.CheckBox();
+            this.isActiveLabel = new System.Windows.Forms.Label();
+            this.addressesListBox = new System.Windows.Forms.ListBox();
+            this.addressesLabel = new System.Windows.Forms.Label();
+            this.addAddressBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(15, 17);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(137, 29);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(158, 14);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(259, 35);
+            this.firstNameTextBox.TabIndex = 1;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(15, 58);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(134, 29);
+            this.lastNameLabel.TabIndex = 0;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(158, 55);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(259, 35);
+            this.lastNameTextBox.TabIndex = 1;
+            // 
+            // isActiveCheckBox
+            // 
+            this.isActiveCheckBox.AutoSize = true;
+            this.isActiveCheckBox.Location = new System.Drawing.Point(158, 106);
+            this.isActiveCheckBox.Name = "isActiveCheckBox";
+            this.isActiveCheckBox.Size = new System.Drawing.Size(15, 14);
+            this.isActiveCheckBox.TabIndex = 2;
+            this.isActiveCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // isActiveLabel
+            // 
+            this.isActiveLabel.AutoSize = true;
+            this.isActiveLabel.Location = new System.Drawing.Point(15, 98);
+            this.isActiveLabel.Name = "isActiveLabel";
+            this.isActiveLabel.Size = new System.Drawing.Size(83, 29);
+            this.isActiveLabel.TabIndex = 0;
+            this.isActiveLabel.Text = "Active:";
+            // 
+            // addressesListBox
+            // 
+            this.addressesListBox.FormattingEnabled = true;
+            this.addressesListBox.ItemHeight = 29;
+            this.addressesListBox.Location = new System.Drawing.Point(20, 179);
+            this.addressesListBox.Name = "addressesListBox";
+            this.addressesListBox.Size = new System.Drawing.Size(397, 207);
+            this.addressesListBox.TabIndex = 3;
+            // 
+            // addressesLabel
+            // 
+            this.addressesLabel.AutoSize = true;
+            this.addressesLabel.Location = new System.Drawing.Point(15, 147);
+            this.addressesLabel.Name = "addressesLabel";
+            this.addressesLabel.Size = new System.Drawing.Size(134, 29);
+            this.addressesLabel.TabIndex = 0;
+            this.addressesLabel.Text = "Addresses:";
+            // 
+            // addAddressBtn
+            // 
+            this.addAddressBtn.Location = new System.Drawing.Point(344, 144);
+            this.addAddressBtn.Name = "addAddressBtn";
+            this.addAddressBtn.Size = new System.Drawing.Size(73, 34);
+            this.addAddressBtn.TabIndex = 4;
+            this.addAddressBtn.Text = "Add";
+            this.addAddressBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(158, 407);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(90, 36);
+            this.saveBtn.TabIndex = 5;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // PersonEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(429, 455);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.addAddressBtn);
+            this.Controls.Add(this.addressesListBox);
+            this.Controls.Add(this.isActiveCheckBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.isActiveLabel);
+            this.Controls.Add(this.addressesLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "PersonEntry";
+            this.Text = "Person Entry Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.CheckBox isActiveCheckBox;
+        private System.Windows.Forms.Label isActiveLabel;
+        private System.Windows.Forms.ListBox addressesListBox;
+        private System.Windows.Forms.Label addressesLabel;
+        private System.Windows.Forms.Button addAddressBtn;
+        private System.Windows.Forms.Button saveBtn;
     }
 }
 
---
+++ CommonProjectTypes/WinFormMiniProjectLibrary/AddressModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormMiniProjectLibrary
+{
+    public class AddressModel
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
