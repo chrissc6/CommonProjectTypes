c2be41b794aa437f519c6c8f5b85c35e7ee40d0d
chris <christopherscaudill@gmail.com>
2020-05-21 10:16:22 -0400

    winform pt2

--- CommonProjectTypes/WinFormsMessageWall/Dashboard.Designer.cs
+++ CommonProjectTypes/WinFormsMessageWall/Dashboard.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.messageListBox = new System.Windows.Forms.ListBox();
+            this.messageListBoxLabel = new System.Windows.Forms.Label();
+            this.addMessageBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(16, 14);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(112, 29);
+            this.messageLabel.TabIndex = 0;
+            this.messageLabel.Text = "Message";
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(134, 11);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(328, 35);
+            this.messageTextBox.TabIndex = 1;
+            // 
+            // messageListBox
+            // 
+            this.messageListBox.FormattingEnabled = true;
+            this.messageListBox.ItemHeight = 29;
+            this.messageListBox.Location = new System.Drawing.Point(21, 113);
+            this.messageListBox.Name = "messageListBox";
+            this.messageListBox.Size = new System.Drawing.Size(441, 381);
+            this.messageListBox.TabIndex = 2;
+            // 
+            // messageListBoxLabel
+            // 
+            this.messageListBoxLabel.AutoSize = true;
+            this.messageListBoxLabel.Location = new System.Drawing.Point(16, 81);
+            this.messageListBoxLabel.Name = "messageListBoxLabel";
+            this.messageListBoxLabel.Size = new System.Drawing.Size(124, 29);
+            this.messageListBoxLabel.TabIndex = 0;
+            this.messageListBoxLabel.Text = "Messages";
+            // 
+            // addMessageBtn
+            // 
+            this.addMessageBtn.Location = new System.Drawing.Point(485, 11);
+            this.addMessageBtn.Name = "addMessageBtn";
+            this.addMessageBtn.Size = new System.Drawing.Size(68, 35);
+            this.addMessageBtn.TabIndex = 3;
+            this.addMessageBtn.Text = "Add";
+            this.addMessageBtn.UseVisualStyleBackColor = true;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(573, 507);
+            this.Controls.Add(this.addMessageBtn);
+            this.Controls.Add(this.messageListBox);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.messageListBoxLabel);
+            this.Controls.Add(this.messageLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Dashboard";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.ListBox messageListBox;
+        private System.Windows.Forms.Label messageListBoxLabel;
+        private System.Windows.Forms.Button addMessageBtn;
     }
 }
 
