acc3bb2fdc8671178cc4f3f7679c293200fb507e
chris <christopherscaudill@gmail.com>
2020-05-25 21:37:31 -0400

    wpf miniproject pt5

--- CommonProjectTypes/WPFCoreMiniProject/MainWindow.xaml.cs
+++ CommonProjectTypes/WPFCoreMiniProject/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace WPFCoreMiniProject
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, ISaveAddress
     {
         BindingList<AddressModel> addresses = new BindingList<AddressModel>();
 
@@ -38,6 +38,33 @@ namespace WPFCoreMiniProject
             //    State = "OH",
             //    ZipCode = "12345"
             //});
+        }
+
+        public void SaveAddress(AddressModel address)
+        {
+            addresses.Add(address);
+        }
+
+        private void addAddressButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddressEntry entry = new AddressEntry(this);
+
+            entry.Show();
+        }
+
+        private void savePerson_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = firstNameTextBox.Text,
+                LastName = lastNameTextBox.Text,
+
+                //IsChecked can return a null, so we use a null coalescing operator (??)
+                //says if the first thing is null then do the next thing instead (false)
+                IsActive = (activeCheckBox.IsChecked ?? false),
+
+                Addresses = addresses.ToList()
+            };
         }
     }
 }
