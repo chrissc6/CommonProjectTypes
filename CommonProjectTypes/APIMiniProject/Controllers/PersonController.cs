b9c2d7a1491652219a8fb0b35efdb4f629315b85
chris <christopherscaudill@gmail.com>
2020-06-01 06:47:14 -0400

    ASP.NET Core API - mini project pt2

---
+++ CommonProjectTypes/APIMiniProject/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIMiniProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIMiniProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] PersonModel pm)
+        {
+            _logger.LogInformation($"Person logged {pm}");
+        }
+    }
+}
