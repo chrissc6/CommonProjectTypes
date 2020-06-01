95b51ecb75a9a7b19eccdfafc029bf64c2e2a9a0
chris <christopherscaudill@gmail.com>
2020-06-01 04:49:18 -0400

    ASP.NET Core MVC pt3

---
+++ CommonProjectTypes/MVCMessageWall/Models/MessageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMessageWall.Models
+{
+    public class MessageModel
+    {
+        public string Message { get; set; }
+    }
+}
