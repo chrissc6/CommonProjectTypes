77b906d222e58b1b937f1e117a206e41fd26fd4b
chris <christopherscaudill@gmail.com>
2020-06-01 06:55:00 -0400

    ASP.NET Core API - mini project pt3

---
+++ CommonProjectTypes/APIMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIMiniProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APIMiniProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST api/Address
+        [HttpPost]
+        public void Post([FromBody] AddressModel pm)
+        {
+            _logger.LogInformation($"Address logged {pm}");
+        }
+    }
+}
