b42ec167394d5d383abb91b5646231527d5daab4
chris <christopherscaudill@gmail.com>
2020-06-01 03:30:50 -0400

    ASP.NET Core Razor Pages pt2

---
+++ CommonProjectTypes/RazorPagesMessageWall/Pages/MessageWall.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMessageWall.Pages
+{
+    public class MessageWallModel : PageModel
+    {
+        //used with asp-for
+        [BindProperty]
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            //will return the same page
+            return Page();
+        }
+    }
+}
