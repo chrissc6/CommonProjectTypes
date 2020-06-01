45ece34a544b2d60dc548c57d6082e330be4c464
chris <christopherscaudill@gmail.com>
2020-06-01 04:58:57 -0400

    ASP.NET Core MVC pt4

--- CommonProjectTypes/MVCMessageWall/Models/MessageModel.cs
+++ CommonProjectTypes/MVCMessageWall/Models/MessageModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace MVCMessageWall.Models
 {
     public class MessageModel
     {
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Message..")]
         public string Message { get; set; }
     }
 }
